95169fd0c85fc3ad531b851b3ed64abc0d97137b
EmptyTeacup-afk <rbeaumont744@gmail.com>
2026-01-15 17:53:44 +0200

    Prove W02 Mission

---
+++ Mission/mission.js
@@ -0,0 +1,22 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = "#121212";
+        logo.setAttribute('src', 'byui-logo-white.png');
+        document.body.style.color = "white";
+
+    } else {
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = "white";
+        logo.setAttribute('src', 'byui-logo-blue.webp');
+        document.body.style.color = "black";
+    }
+}           
+                    
